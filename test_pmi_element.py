7e91549abc8d185deb231c937d7740606f9454ec
jasonwbw <384444165@163.com>
2013-11-11 16:05:56 +0800

    add pmi element unit test

---
+++ test_pmi_element.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# The unit test case for pmi.TopkHeap and PMIElement
+#
+# @author: Jason Wu (Jasonwbw@yahoo.com)
+
+from pmi import PMIElement
+import sys
+import unittest
+
+class PMIElementTestCase(unittest.TestCase):  
+    def setUp(self):  
+      pass
+          
+    def tearGt(self):  
+      f = PMIElement('f', 12)
+      e = PMIElement('e', 11)
+      self.assertEqual(True, e < f)
+      self.assertEqual(True, f > e)
+    
+    def testEq(self): 
+      f = PMIElement('f', 11)
+      e = PMIElement('e', 11)
+      g = PMIElement('e', 11)
+      self.assertEqual(False, e == f)
+      self.assertEqual(True, e == g)
+
+
+    def testPrintSomething(self):
+      pass
+
+if __name__ == "__main__":  
+    unittest.main()  
