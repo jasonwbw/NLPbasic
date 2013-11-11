8a759b11b8877eca06c47749e1fafe4f1fec59cf
jasonwbw <384444165@163.com>
2013-11-11 16:09:32 +0800

    edit pmi
    1. rich the element. 2. fix the bug that TopkHeap return the reversed
    list. 3. fix the log0 bug of PMI

--- pmi.py
+++ pmi.py
@@ -15,7 +15,18 @@ class PMIElement(object):
 		self.pmi = pmi
 
 	def __gt__(self, element):
-		return self.pmi > element.pmi
+		if isinstance(element, PMIElement):
+			return self.pmi > element.pmi
+		raise TypeError
+
+	def __eq__(self, other):  
+		if isinstance(other, PMIElement) and \
+			other.t2 == self.t2 and other.pmi == self.pmi:
+			return True
+		return False
+
+	def __str__(self):
+		return self.t2 + ":" + str(self.pmi)
 
 
 class TopkHeap(object):
@@ -32,14 +43,14 @@ class TopkHeap(object):
                 heapq.heapreplace(self.data, elem) 
 
     def topK(self):
-    	return sorted(self.data)
+    	return sorted(self.data)[::-1]
     	# tem_data = self.data
         # return [x for x in reversed([heapq.heappop(tem_data) for x in xrange(len(tem_data))])]
 
 
 class PMI(object):
 	def __init__(self, inverted_index, top = 50):
-		self.inverted_index = inverted_index
+		self.iindex = inverted_index
 		self.top = top
 		self.term_pmi = {}
 
@@ -47,17 +58,23 @@ class PMI(object):
 		self.inverted_index = inverted_index
 
 	def build(self):
-		terms = self.inverted_index.get_terms()
+		inf = -float('inf')
+		terms = self.iindex.get_terms()
 		for term in terms:
-			self.term_pmi[term] = TopkHeap(top)
-		for i in range(len(terms)):
-			for j in range(i, len(terms)):
+			self.term_pmi[term] = TopkHeap(self.top)
+		for i in range(len(terms)-1):
+			for j in range(i+1, len(terms)):
 				# PMI(t1, t2) = log(p(t1,t2)/(p(t1)p(t2)))
-				pmi = math.log(self.inverted_index.concurrence(term[i], term[j]) \
-					/(self.inverted_index.get_word_appear(term[i]) \
-						* self.inverted_index.get_word_appear(term[j])),  2)
+				to_log = self.iindex.concurrence(terms[i], terms[j]) \
+					/(self.iindex.get_word_appear(terms[i]) \
+						* self.iindex.get_word_appear(terms[j]) \
+						/ self.iindex.get_num_docs())
+				if to_log == 0:
+					pmi = inf
+				else:
+					pmi = math.log(to_log,  2)
 				self.term_pmi[terms[i]].push(PMIElement(terms[j], pmi))
 				self.term_pmi[terms[j]].push(PMIElement(terms[i], pmi))
 
 	def get_top_pmi(self, term):
-		return self.term_pmi[term]
+		return self.term_pmi[term].topK()
