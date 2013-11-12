fe62e3c32b879f4763257fc34f54480ba68da82a
jasonwbw <384444165@163.com>
2013-11-12 16:29:08 +0800

    change the package structure

---
+++ nlp_basic/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# @author: Jason Wu (Jasonwbw@yahoo.com)
+
+import inverted_index
+import pmi
+
+__all__ = ['inverted_index', 'pmi']
---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# @author: Jason Wu (Jasonwbw@yahoo.com)
+
+import test_inverted_index
+import test_pmi
+import test_pmi_element
+import test_pmi_topkheap
+
+__all__ = ['test_inverted_index', 'test_pmi', 'test_pmi_element', 'test_pmi_topkheap']
