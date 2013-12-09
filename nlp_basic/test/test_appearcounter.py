43d39c5c658ac4a6c7599588bf3709bf170dca29
jasonwbw <384444165@163.com>
2013-12-09 16:43:36 +0800

    edit method's unit test for appearcounter
    add more method's unit test for appearcounter, and build more method
    unit test needed complete the code

--- nlp_basic/test/test_appearcounter.py
+++ nlp_basic/test/test_appearcounter.py
@@ -38,7 +38,34 @@ class AppearCountTestCase(unittest.TestCase):
 		self.assertEqual(3, self.ac.get_term_appear_docs('e'))
 		self.assertEqual(4, self.ac.get_term_appear_docs('f'))
 		self.assertEqual(0, self.ac.get_term_appear_docs('m'))
+	def testGetTermAppearDocs(self):
+		self.assertEqual(4, self.ac.get_term_appear_docs('a'))
+		self.assertEqual(0, self.ac.get_term_appear_docs('b'))
+		self.assertEqual(1, self.ac.get_term_appear_docs('c'))
+		self.assertEqual(3, self.ac.get_term_appear_docs('d'))
+		self.assertEqual(3, self.ac.get_term_appear_docs('e'))
+		self.assertEqual(4, self.ac.get_term_appear_docs('f'))
+		self.assertEqual(0, self.ac.get_term_appear_docs('m'))
 
+	def testCountLessAppear(self):
+		self.assertEqual(1, self.ac.count_less_appear(1)) #b
+		self.assertEqual(2, self.ac.count_less_appear(2)) #b,c
+		self.assertEqual(2, self.ac.count_less_appear(3)) #b,c
+		self.assertEqual(5, self.ac.count_less_appear(5)) #b,c,d,e,f
+
+	def testGetWordsDoccountLessAppear(self):
+		self.assertEqual(set(['b']), set(self.ac.get_words_doccount_less_appear(1))) #b
+		self.assertEqual(set(['b', 'c']), set(self.ac.get_words_doccount_less_appear(2))) #b,c
+		self.assertEqual(set(['b', 'c']), set(self.ac.get_words_doccount_less_appear(3))) #b,c
+		self.assertEqual(set(['b', 'c', 'd', 'e']), set(self.ac.get_words_doccount_less_appear(4))) #b,c,d,e
+		self.assertEqual(set(['a', 'b', 'c', 'd', 'e', 'f']), set(self.ac.get_words_doccount_less_appear(5))) #a,b,c,d,e,f
+
+	def testGetWordLessAppear(self):
+		self.assertEqual(set(['b']), set(self.ac.get_words_less_appear(1))) #b
+		self.assertEqual(set(['b', 'c']), set(self.ac.get_words_less_appear(2))) #b,c
+		self.assertEqual(set(['b', 'c']), set(self.ac.get_words_less_appear(3))) #b,c
+		self.assertEqual(set(['b', 'c', 'd', 'e', 'f']), set(self.ac.get_words_less_appear(5))) #b,c,d,e,f
+	
 	def testPrintSomething(self):
 		pass
 
